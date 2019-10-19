1018117b101e3f00bce968d4e4fff7792615a1d0
Eric Tobin <eatobin@gmail.com>
2019-10-19 17:38:19 -0500

    add seed files

---
+++ src/main/kotlin/redpointKotlin/App.kt
@@ -0,0 +1,9 @@
+package redpointKotlin
+
+object App {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+}
---
+++ src/main/kotlin/redpointKotlin/Package.kt
@@ -0,0 +1,11 @@
+package redpointKotlin
+
+//typealias Name = String
+//typealias MaxBooks = Int
+//
+//typealias Title = String
+//typealias Author = String
+//
+//typealias Borrowers = List<Borrower>
+//typealias Books = List<Book>
+//typealias jsonString = String
