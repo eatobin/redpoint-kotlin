32cbcecd404f72b7661131f1a93a4d6586501ece
Eric Tobin <eatobin@gmail.com>
2019-10-21 16:04:41 -0500

    rosterTest progress!!!

--- src/test/kotlin/redpointKotlin/RosterTest.kt
+++ src/test/kotlin/redpointKotlin/RosterTest.kt
@@ -3,15 +3,21 @@ package redpointKotlin
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-private val ringo = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
-//private val roster = Roster("The Beatles", 2014,)
+private val rinSta: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
+private val johLen: Player = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
+private val geoHar: Player = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
+private val pauMcc: Player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
 
-//class BorrowerTest : StringSpec({
+private val players: Players = mapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHar, "PauMcc" to pauMcc)
 
-//    "getName should return the Borrower name" {
-//        getName(br1).shouldBe("Borrower1")
-//    }
-//
+private val roster: Roster = Roster("The Beatles", 2014, players)
+
+class RosterTest : StringSpec({
+
+    "rosterName should return \"The Beatles\"" {
+        roster.rosterName.shouldBe("The Beatles")
+    }
+
 //    "setName should set the Borrower name" {
 //        setName("Borrower1", Borrower("Jack", 1)).shouldBe(br1)
 //    }
@@ -28,4 +34,4 @@ private val ringo = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
 //        borrowerToString(br1).shouldBe("Borrower1 (1 books)")
 //    }
 
-//})
+})
