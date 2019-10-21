a186baa970079b6a149ad08b0cc5962f9c73f110
Eric Tobin <eatobin@gmail.com>
2019-10-21 16:07:45 -0500

    rosterTest progress!!!

--- src/test/kotlin/redpointKotlin/RosterTest.kt
+++ src/test/kotlin/redpointKotlin/RosterTest.kt
@@ -18,20 +18,8 @@ class RosterTest : StringSpec({
         roster.rosterName.shouldBe("The Beatles")
     }
 
-//    "setName should set the Borrower name" {
-//        setName("Borrower1", Borrower("Jack", 1)).shouldBe(br1)
-//    }
-//
-//    "getMaxBooks should return the Borrower maxBooks" {
-//        getMaxBooks(br1).shouldBe(1)
-//    }
-//
-//    "setMaxBooks should set the Borrower maxBooks" {
-//        setMaxBooks(11, br1).shouldBe(Borrower("Borrower1", 11))
-//    }
-//
-//    "the Borrower string should print" {
-//        borrowerToString(br1).shouldBe("Borrower1 (1 books)")
-//    }
+    "rosterYear should return 2014" {
+        roster.rosterYear.shouldBe(2014)
+    }
 
 })
