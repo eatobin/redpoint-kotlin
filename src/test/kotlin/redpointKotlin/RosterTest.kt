691aa39abb81a4a14ff2fea351c4de337c761ca9
Eric Tobin <eatobin@gmail.com>
2019-10-21 15:41:43 -0500

    rosterTest progress

---
+++ src/test/kotlin/redpointKotlin/RosterTest.kt
@@ -0,0 +1,31 @@
+package redpointKotlin
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+private val ringo = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
+//private val roster = Roster("The Beatles", 2014,)
+
+//class BorrowerTest : StringSpec({
+
+//    "getName should return the Borrower name" {
+//        getName(br1).shouldBe("Borrower1")
+//    }
+//
+//    "setName should set the Borrower name" {
+//        setName("Borrower1", Borrower("Jack", 1)).shouldBe(br1)
+//    }
+//
+//    "getMaxBooks should return the Borrower maxBooks" {
+//        getMaxBooks(br1).shouldBe(1)
+//    }
+//
+//    "setMaxBooks should set the Borrower maxBooks" {
+//        setMaxBooks(11, br1).shouldBe(Borrower("Borrower1", 11))
+//    }
+//
+//    "the Borrower string should print" {
+//        borrowerToString(br1).shouldBe("Borrower1 (1 books)")
+//    }
+
+//})
