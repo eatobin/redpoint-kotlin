ea61307bb6b3f453ad36484ba566e150b89b693c
Eric Tobin <eatobin@gmail.com>
2019-10-22 08:43:21 -0500

    Roster progress

--- src/main/kotlin/redpointKotlin/Roster.kt
+++ src/main/kotlin/redpointKotlin/Roster.kt
@@ -5,14 +5,18 @@ data class GiftPair(val givee: Givee, val giver: Giver)
 data class Player(val playerName: PlayerName, val giftHistory: GiftHistory)
 
 data class Roster(val rosterName: RosterName, val rosterYear: RosterYear, val players: Players) {
+
     companion object {
+
         fun getPlayer(roster: Roster, playerKey: PlayerKey): Player? = roster.players[playerKey]
 
-        fun getGiftPairInRoster(roster: Roster, playerKey: PlayerKey, giftYear: GiftYear): GiftPair? {
+        fun getGiftPairInRoster(roster: Roster, playerKey: PlayerKey, giftYear: GiftYear): GiftPair {
             val mPlayer = getPlayer(roster, playerKey)
             return if (mPlayer != null) {
                 mPlayer.giftHistory[giftYear]
-            } else null
+            } else GiftPair("null", "null")
         }
+
     }
+
 }
