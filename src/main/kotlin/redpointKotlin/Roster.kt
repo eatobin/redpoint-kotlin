2b2ffb504c8cc0673e8a278043b32a92e0dbfcda
Eric Tobin <eatobin@gmail.com>
2019-10-23 15:59:56 -0500

    to pottery!!

--- src/main/kotlin/redpointKotlin/Roster.kt
+++ src/main/kotlin/redpointKotlin/Roster.kt
@@ -6,21 +6,20 @@ data class Player(val playerName: PlayerName, val giftHistory: GiftHistory) {
 
     companion object {
 
-        fun <E> Iterable<E>.updated(index: Int, elem: E) =
-            mapIndexed { i, existing -> if (i == index) elem else existing }
-
         fun getGiftPairInGiftHistory(giftHistory: GiftHistory, giftYear: GiftYear): GiftPair =
             giftHistory[giftYear]
 
-        fun setGiftPairInGiftHistory(giftHistory: GiftHistory, giftYear: GiftYear, giftPair: GiftPair): GiftHistory =
-            giftHistory.updated(giftYear, giftPair)
+        fun setGiftPairInGiftHistory(giftHistory: GiftHistory, giftYear: GiftYear, giftPair: GiftPair): GiftHistory {
+            giftHistory[giftYear] = giftPair
+            return giftHistory
+        }
 
         fun setGiftHistoryInPlayer(player: Player, giftHistory: GiftHistory): Player =
             player.copy(giftHistory = giftHistory)
 
         fun addYearInPlayer(player: Player, playerKey: PlayerKey): Player {
-            val ngh = player.giftHistory.plusElement(GiftPair(playerKey, playerKey))
-            return setGiftHistoryInPlayer(player, ngh)
+            player.giftHistory.add(GiftPair(playerKey, playerKey))
+            return setGiftHistoryInPlayer(player, player.giftHistory)
         }
 
     }
@@ -71,10 +70,7 @@ data class Roster(val rosterName: RosterName, val rosterYear: RosterYear, val pl
 //            val mPlayer = getPlayerInRoster(roster, playerKey)
 //            return if (mPlayer != null) {
 //                val gh = mPlayer.giftHistory
-//                val ngh = Player.setGiftPairInGiftHistory(gh, giftYear, giftPair)
-//                val nPlayer = Player.setGiftHistoryInPlayer(mPlayer, ngh)
-//                val mutPlayers = roster.players.toMutableMap()
-//                mutPlayers.put(playerKey, nPlayer)
+//                val ngh =
 //
 //            } else roster
 //        }
@@ -84,7 +80,6 @@ data class Roster(val rosterName: RosterName, val rosterYear: RosterYear, val pl
 //            val giftPair: GiftPair = GiftPair(givee, giver)
 //            return setGift
 //        }
-
 
     }
 
