b80f9566f3a04a88a2484c8d44c3b184d996bf5e
Eric Tobin <eatobin@gmail.com>
2019-11-05 17:27:19 -0600

    gifthistory tests!!

--- src/test/kotlin/redpointKotlin/GiftHistoryTest.kt
+++ src/test/kotlin/redpointKotlin/GiftHistoryTest.kt
@@ -4,11 +4,11 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import redpointKotlin.GiftHistory.addYear
 import redpointKotlin.GiftHistory.getGiftPair
-
-val giftHistory: GiftHistoryT = mutableListOf(GiftPair("JohLen", "GeoHar"))
+import redpointKotlin.GiftHistory.setGiftPair
 
 class GiftHistoryTest : StringSpec({
     "A GiftHistory should add a new year" {
+        val giftHistory: GiftHistoryT = mutableListOf(GiftPair("JohLen", "GeoHar"))
         addYear(giftHistory, "NewBee").shouldBe(
             mutableListOf(
                 GiftPair("JohLen", "GeoHar"),
@@ -18,15 +18,12 @@ class GiftHistoryTest : StringSpec({
     }
 
     "A GiftHistory should return a giftPair" {
+        val giftHistory: GiftHistoryT = mutableListOf(GiftPair("JohLen", "GeoHar"))
         getGiftPair(giftHistory, 0).shouldBe(GiftPair("JohLen", "GeoHar"))
     }
 
-//    "A GiftPair should return its giver" {
-//        giftPair.giver.shouldBe("GeoHar")
-//    }
-//
-//    "A GiftPair should return an updated givEeEr" {
-//        setGivEeEr(giftPair, "NewBee", "ee").shouldBe(GiftPair("NewBee", "GeoHar"))
-//        setGivEeEr(giftPair, "NewBee", "er").shouldBe(GiftPair("JohLen", "NewBee"))
-//    }
+    "A GiftHistory should return an updated giftHistory" {
+        val giftHistory: GiftHistoryT = mutableListOf(GiftPair("JohLen", "GeoHar"))
+        setGiftPair(giftHistory, 0, GiftPair("me", "you")).shouldBe(mutableListOf(GiftPair("me", "you")))
+    }
 })
