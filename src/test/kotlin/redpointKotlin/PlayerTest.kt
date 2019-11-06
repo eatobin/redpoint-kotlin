31b14b6c296f924eb55f3a47df5f53ebda2f64cd
Eric Tobin <eatobin@gmail.com>
2019-11-06 16:02:26 -0600

    player progress

--- src/test/kotlin/redpointKotlin/PlayerTest.kt
+++ src/test/kotlin/redpointKotlin/PlayerTest.kt
@@ -2,7 +2,7 @@ package redpointKotlin
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import redpointKotlin.GiftHistory.addYear
+import redpointKotlin.Player.Companion.setGiftHistory
 
 val player: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
 
@@ -11,9 +11,16 @@ class PlayerTest : StringSpec({
         player.playerName.shouldBe("Ringo Starr")
     }
 
-    "A Player should return its gifthistory" {
+    "A Player should return its giftHistory" {
         player.giftHistory.shouldBe(listOf(GiftPair("JohLen", "GeoHar")))
     }
 
-
+    "A Player should return an updated giftHistory" {
+        setGiftHistory(player, listOf(GiftPair("nope", "yup"))).shouldBe(
+            Player(
+                "Ringo Starr",
+                listOf(GiftPair("nope", "yup"))
+            )
+        )
+    }
 })
