d5351575c85c6a09988e4de69d0f2d403356b9e4
Eric Tobin <eatobin@gmail.com>
2023-02-08 21:35:30 -0700

    bump gradle 7.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.8.0"
     application
 }
 
@@ -21,8 +19,8 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
