bab140312d6ce0f3505f1f786dfbc868aad4337c
Eric Tobin <eatobin@gmail.com>
2023-02-11 18:28:59 -0700

    val

--- src/main/kotlin/GiftPair.kt
+++ src/main/kotlin/GiftPair.kt
@@ -2,7 +2,7 @@ import com.google.gson.Gson
 
 data class GiftPair(val givee: Givee, val giver: Giver) {
     companion object {
-        private var gson = Gson()
+        private val gson = Gson()
         fun giftPairUpdateGivee(givee: Givee, giftPair: GiftPair): GiftPair = giftPair.copy(givee = givee)
         fun giftPairUpdateGiver(giver: Giver, giftPair: GiftPair): GiftPair = giftPair.copy(giver = giver)
         fun giftPairJsonStringToGiftPair(jsonString: JsonString): GiftPair =
