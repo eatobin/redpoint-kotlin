e5df7294bf3cf30a615b466b403956653daa5292
Eric Tobin <eatobin@gmail.com>
2023-02-14 15:16:20 -0700

    uberJar!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
 }
 
-group = "eatobin.com"
+group = "com.eatobin"
 version = "1.0"
 
 repositories {
@@ -17,6 +17,16 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.create("uberJar", Jar::class) {
+    group = "build"
+    description = "Creates a self-contained fat JAR of the application that can be run."
+    manifest.attributes["Main-Class"] = "MainKt"
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
+    from(dependencies)
+    with(tasks.jar.get())
 }
 
 kotlin {
