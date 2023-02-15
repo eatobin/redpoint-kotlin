80c31c528eb558d710279edc55cca5b0158cdc50
Eric Tobin <eatobin@gmail.com>
2023-02-15 11:56:32 -0700

    type-alias is back

--- src/main/kotlin/GiftHistory.kt
+++ src/main/kotlin/GiftHistory.kt
@@ -1,15 +1,20 @@
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
+typealias GiftYear = Int
+
 object GiftHistory {
     fun giftHistoryJsonStringToGiftHistory(jsonString: String): List<GiftPair> = Json.decodeFromString(jsonString)
 
     fun giftHistoryAddYear(giftHistory: List<GiftPair>, playerKey: String): List<GiftPair> =
         giftHistory.plus(GiftPair(playerKey, playerKey))
 
-    fun giftHistoryUpdateGiftHistory(giftHistory: List<GiftPair>, giftYear: Int, giftPair: GiftPair): List<GiftPair> {
+    fun giftHistoryUpdateGiftHistory(
+        giftHistory: List<GiftPair>, giftYear: GiftYear, giftPair: GiftPair
+    ): List<GiftPair> {
         val mgh = giftHistory.toMutableList()
         mgh[giftYear] = giftPair
         return mgh.toList()
     }
 }
+
--- src/main/kotlin/GiftPair.kt
+++ src/main/kotlin/GiftPair.kt
@@ -2,11 +2,16 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
+typealias PlayerKey = String
+typealias Givee = PlayerKey
+typealias Giver = PlayerKey
+typealias JsonString = String
+
 @Serializable
-data class GiftPair(val givee: String, val giver: String) {
+data class GiftPair(val givee: Givee, val giver: Giver) {
     companion object {
-        fun giftPairJsonStringToGiftPair(jsonString: String): GiftPair = Json.decodeFromString(jsonString)
-        fun giftPairUpdateGivee(givee: String, giftPair: GiftPair): GiftPair = giftPair.copy(givee = givee)
-        fun giftPairUpdateGiver(giver: String, giftPair: GiftPair): GiftPair = giftPair.copy(giver = giver)
+        fun giftPairJsonStringToGiftPair(jsonString: JsonString): GiftPair = Json.decodeFromString(jsonString)
+        fun giftPairUpdateGivee(givee: Givee, giftPair: GiftPair): GiftPair = giftPair.copy(givee = givee)
+        fun giftPairUpdateGiver(giver: Giver, giftPair: GiftPair): GiftPair = giftPair.copy(giver = giver)
     }
 }
