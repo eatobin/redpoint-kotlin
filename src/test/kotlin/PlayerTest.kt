155c9815b54407acc9d1d1d83727ace5335605a7
Eric Tobin <eatobin@gmail.com>
2023-02-16 12:31:46 -0700

    kotest works with player only

--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -1,22 +1,10 @@
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.startWith
-
-class MyTests : StringSpec({
-    "length should return size of string" {
-        "hello".length shouldBe 5
-    }
-    "startsWith should test for a prefix" {
-        "world" should startWith("wor")
-    }
-})
-
 
 val player: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
 
 class PlayerTest : StringSpec({
-    "A Player should return its playerName" {
+    "player should return its playerName" {
         player.playerName.shouldBe("Ringo Starr")
     }
 })
