b93f056afd10a48c055944c8c83f559ce83ee865
Eric Tobin <eatobin@gmail.com>
2023-02-16 17:54:40 -0700

    kotest progress

--- src/test/kotlin/GiftPairTest.kt
+++ src/test/kotlin/GiftPairTest.kt
@@ -1,17 +1,23 @@
-// import GiftPair.Companion.giftPairJsonStringToGiftPair
-// import GiftPair.Companion.giftPairUpdateGivee
-// import GiftPair.Companion.giftPairUpdateGiver
-// import kotlin.test.Test
-// import kotlin.test.assertEquals
+import GiftPair.Companion.giftPairJsonStringToGiftPair
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-// private const val JSON_STRING: String = "{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}"
-// private val giftPair: GiftPair = GiftPair("GeoHar", "JohLen")
+private const val JSON_STRING: String = "{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}"
+private const val BAD_JSON_STRING: String = "{\"givee\"\"GeoHar\",\"giver\":\"JohLen\"}"
+private val giftPair: GiftPair = GiftPair("GeoHar", "JohLen")
 
-// class GiftPairTest {
-//     @Test
-//     fun testGiftPairJsonStringToGiftPair() {
-//         assertEquals(giftPair, giftPairJsonStringToGiftPair(JSON_STRING))
-//     }
+class GiftPairTest : StringSpec({
+    "giftPair should build from JSON" {
+        giftPairJsonStringToGiftPair(JSON_STRING).shouldBe(giftPair)
+    }
+    "giftPair should NOT build from BAD JSON" {
+        shouldThrowAny {
+            giftPairJsonStringToGiftPair(BAD_JSON_STRING)
+        }
+    }
+})
+
 
 //     @Test
 //     fun testGiftPairUpdateGivee() {
