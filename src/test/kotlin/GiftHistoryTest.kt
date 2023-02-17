95170b01fdcdd3bba363d09867529ffb4c3e71be
Eric Tobin <eatobin@gmail.com>
2023-02-16 18:11:29 -0700

    GiftHistoryTest.kt tests

--- src/test/kotlin/GiftHistoryTest.kt
+++ src/test/kotlin/GiftHistoryTest.kt
@@ -1,23 +1,33 @@
-// import GiftHistory.giftHistoryAddYear
-// import GiftHistory.giftHistoryJsonStringToGiftHistory
-// import GiftHistory.giftHistoryUpdateGiftHistory
-// import kotlin.test.Test
-// import kotlin.test.assertEquals
+import GiftHistory.giftHistoryAddYear
+import GiftHistory.giftHistoryJsonStringToGiftHistory
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-// private const val JSON_STRING: String = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
-// private val giftHistory: List<GiftPair> = listOf(GiftPair("GeoHar", "JohLen"))
+private const val JSON_STRING: String = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+private const val BAD_JSON_STRING: String = "[{\"givee\"\"GeoHar\",\"giver\":\"JohLen\"}]"
+private val giftHistory: List<GiftPair> = listOf(GiftPair("GeoHar", "JohLen"))
 
-// class GiftHistoryTest {
-//     @Test
-//     fun testGiftHistoryJsonStringToGiftHistory() {
-//         assertEquals(giftHistory, giftHistoryJsonStringToGiftHistory(JSON_STRING))
-//     }
+class GiftHistoryTest : StringSpec({
+    "giftHistory should build from JSON" {
+        giftHistoryJsonStringToGiftHistory(JSON_STRING).shouldBe(giftHistory)
+    }
+    "giftHistory should NOT build from BAD JSON" {
+        shouldThrowAny {
+            giftHistoryJsonStringToGiftHistory(BAD_JSON_STRING)
+        }
+    }
+    "giftHistory should add a year" {
+        giftHistoryAddYear(giftHistory, "NewBee")
+            .shouldBe(listOf(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
+    }
+})
+
 
 //     @Test
 //     fun testGiftHistoryAddYear() {
 //         assertEquals(
-//             listOf(
-//                 GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")
+//             listOf(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")
 //             ), giftHistoryAddYear(giftHistory, "NewBee")
 //         )
 //     }
