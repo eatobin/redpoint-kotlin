9202d5f58e59cd7e242b6dd58230975fca4cb0b7
Eric Tobin <eatobin@gmail.com>
2023-02-19 17:21:20 -0700

    playersGetPlayerName

--- src/test/kotlin/PlayersTest.kt
+++ src/test/kotlin/PlayersTest.kt
@@ -31,59 +31,7 @@ class PlayersTest : StringSpec({
             newBeePlayers
         )
     }
+    "players should return a player name" {
+        playersGetPlayerName("PauMcc", players).shouldBe("Paul McCartney")
+    }
 })
-
-//"Players" should "return an updated player" in {
-//    assert(playersUpdatePlayer("RinSta")(Player("New Bee", Vector(GiftPair("NewBee", "NewBee"))))(players) == newBeePlayers)
-//}
-
-//     "Players should return an extended year players" {
-//         val rinSta = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
-//         val johLen = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
-//         val geoHar = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
-//         val pauMcc = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
-//         val players: PlayersT =
-//             mutableMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHar, "PauMcc" to pauMcc)
-
-//         val rinStaExt =
-//             Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar"), GiftPair("RinSta", "RinSta")))
-//         val johLenExt =
-//             Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta"), GiftPair("JohLen", "JohLen")))
-//         val geoHarExt =
-//             Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc"), GiftPair("GeoHar", "GeoHar")))
-//         val pauMccExt =
-//             Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen"), GiftPair("PauMcc", "PauMcc")))
-//         val playersExt: PlayersT =
-//             mutableMapOf("RinSta" to rinStaExt, "JohLen" to johLenExt, "GeoHar" to geoHarExt, "PauMcc" to pauMccExt)
-
-//         addYearPlayers(players).shouldBe(playersExt)
-//     }
-
-//     "Players should return a player's name" {
-//         getPlayerNamePlayers(players, "GeoHar").shouldBe("George Harrison")
-//     }
-
-//     "Players should return a givEeEr" {
-//         getGivEeErPlayers(players, "GeoHar", 0, "ee").shouldBe("RinSta")
-//         getGivEeErPlayers(players, "GeoHar", 0, "er").shouldBe("PauMcc")
-//     }
-
-//     "Players should set a givEeEr" {
-//         val rinSta = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
-//         val johLen = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
-//         val geoHar = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
-//         val pauMcc = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
-//         val players: PlayersT =
-//             mutableMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHar, "PauMcc" to pauMcc)
-
-//         val geoHarGivee = Player("George Harrison", listOf(GiftPair("you", "PauMcc")))
-//         val geoHarGiver = Player("George Harrison", listOf(GiftPair("you", "you")))
-//         val playersGivee: PlayersT =
-//             mutableMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGivee, "PauMcc" to pauMcc)
-//         val playersGiver: PlayersT =
-//             mutableMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGiver, "PauMcc" to pauMcc)
-
-//         setGivEeErPlayers(players, "GeoHar", 0, "you", "ee").shouldBe(playersGivee)
-//         setGivEeErPlayers(players, "GeoHar", 0, "you", "er").shouldBe(playersGiver)
-//     }
-
