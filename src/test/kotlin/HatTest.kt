6abd0182746fe2294252745027ba9f46b563296e
Eric Tobin <eatobin@gmail.com>
2023-03-06 20:11:53 -0700

    hat test progress

--- src/test/kotlin/HatTest.kt
+++ src/test/kotlin/HatTest.kt
@@ -1,12 +1,18 @@
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import java.util.*
 
 private const val JSON_STRING: JsonStringTA = "[\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
 private const val BAD_JSON_STRING: JsonStringTA = "[\"RinSta\"\"JohLen\"\"GeoHar\"\"PauMcc\"]"
 private const val DUPLICATES: JsonStringTA = "[\"RinSta\",\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
-private val testHat: SortedSet<PlayerKeyTA> = sortedSetOf("JohLen", "RinSta", "PauMcc", "GeoHar")
+private val testHat: HatTA = sortedSetOf("JohLen", "RinSta", "PauMcc", "GeoHar")
+
+private val rinSta: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
+private val johLen: Player = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
+private val geoHar: Player = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
+private val pauMcc: Player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
+private val players: PlayersTA =
+    sortedMapOf("PauMcc" to pauMcc, "GeoHar" to geoHar, "RinSta" to rinSta, "JohLen" to johLen)
 
 class HatTest : StringSpec({
     "hat should build from JSON" {
@@ -19,5 +25,9 @@ class HatTest : StringSpec({
     }
     "hat should NOT build a non-Set from DUPLICATES" {
         hatJsonStringToHat(DUPLICATES).shouldBe(testHat)
+    }
+    "hat should remove a puck" {
+        hatRemovePuck("RinSta", testHat).shouldBe(sortedSetOf("JohLen", "PauMcc", "GeoHar"))
+        hatRemovePuck("RinStaX", sortedSetOf()).shouldBe(sortedSetOf())
     }
 })
