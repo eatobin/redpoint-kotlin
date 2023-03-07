1102ecb598550bce5a0269f288362e7b9bc6eebd
Eric Tobin <eatobin@gmail.com>
2023-03-06 20:00:08 -0700

    hat progress

--- src/main/kotlin/Hat.kt
+++ src/main/kotlin/Hat.kt
@@ -9,3 +9,11 @@ fun hatJsonStringToHat(jsonString: JsonStringTA): HatTA {
     val myUnsortedSet: Set<PlayerKeyTA> = Json.decodeFromString(jsonString)
     return myUnsortedSet.toSortedSet()
 }
+
+fun hatMakeHat(players: PlayersTA): HatTA = players.keys.toSortedSet()
+
+fun hatRemovePuck(playerKey: PlayerKeyTA, hat: HatTA): HatTA = hat.minus(playerKey).toSortedSet()
+
+fun hatDiscardGivee(givee: GiveeTA, discards: DiscardsTA): HatTA = discards.plus(discards).toSortedSet()
+
+fun hatReturnDiscards(discards: DiscardsTA, geHat: HatTA): HatTA = geHat.plus(geHat).toSortedSet()
