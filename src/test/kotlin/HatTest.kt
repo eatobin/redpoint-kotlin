f0a0e8c0d6cdb0eb6bc9fc5a94be8a2283a8b670
Eric Tobin <eatobin@gmail.com>
2023-03-09 16:40:19 -0700

    reformat vals

--- src/test/kotlin/HatTest.kt
+++ src/test/kotlin/HatTest.kt
@@ -2,29 +2,28 @@ import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-private const val JSON_STRING: JsonStringTA = "[\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
-private const val BAD_JSON_STRING: JsonStringTA = "[\"RinSta\"\"JohLen\"\"GeoHar\"\"PauMcc\"]"
-private const val DUPLICATES: JsonStringTA = "[\"RinSta\",\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
-private val testHat: HatTA = sortedSetOf("JohLen", "RinSta", "PauMcc", "GeoHar")
-
-private val rinSta: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
-private val johLen: Player = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
-private val geoHar: Player = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
-private val pauMcc: Player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
-private val players: PlayersTA =
-    sortedMapOf("PauMcc" to pauMcc, "GeoHar" to geoHar, "RinSta" to rinSta, "JohLen" to johLen)
-
 class HatTest : StringSpec({
+    val jsonString: JsonStringTA = "[\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
+    val badJsonString: JsonStringTA = "[\"RinSta\"\"JohLen\"\"GeoHar\"\"PauMcc\"]"
+    val duplicates: JsonStringTA = "[\"RinSta\",\"RinSta\",\"JohLen\",\"GeoHar\",\"PauMcc\"]"
+    val testHat: HatTA = sortedSetOf("JohLen", "RinSta", "PauMcc", "GeoHar")
+
+    val rinSta = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
+    val johLen = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
+    val geoHar = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
+    val pauMcc = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
+    val players: PlayersTA = sortedMapOf("PauMcc" to pauMcc, "GeoHar" to geoHar, "RinSta" to rinSta, "JohLen" to johLen)
+
     "hat should build from JSON" {
-        hatJsonStringToHat(JSON_STRING).shouldBe(testHat)
+        hatJsonStringToHat(jsonString).shouldBe(testHat)
     }
     "hat should NOT build a sortedSet from BAD JSON" {
         shouldThrowAny {
-            hatJsonStringToHat(BAD_JSON_STRING)
+            hatJsonStringToHat(badJsonString)
         }
     }
     "hat should NOT build a non-Set from DUPLICATES" {
-        hatJsonStringToHat(DUPLICATES).shouldBe(testHat)
+        hatJsonStringToHat(duplicates).shouldBe(testHat)
     }
     "hat should remove a puck" {
         hatRemovePuck("RinSta", testHat).shouldBe(sortedSetOf("JohLen", "PauMcc", "GeoHar"))
