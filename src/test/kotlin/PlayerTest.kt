4ff87aec6d69c99a02461282a440d23de2495613
Eric Tobin <eatobin@gmail.com>
2023-03-09 16:21:01 -0700

    reformat vals

--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -4,26 +4,26 @@ import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-private const val JSON_STRING: JsonStringTA =
-    "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-private const val BAD_JSON_STRING: JsonStringTA =
-    "{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-private const val BAD_JSON_STRING_2: JsonStringTA =
-    "{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
-val player: Player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
-
 class PlayerTest : StringSpec({
+    val jsonString: JsonStringTA =
+        "{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+    val badJsonString: JsonStringTA =
+        "{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+    val badJsonString2: JsonStringTA =
+        "{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}"
+    val player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
+
     "player should build from JSON" {
-        playerJsonStringToPlayer(JSON_STRING).shouldBe(player)
+        playerJsonStringToPlayer(jsonString).shouldBe(player)
     }
     "player should NOT build from BAD JSON" {
         shouldThrowAny {
-            playerJsonStringToPlayer(BAD_JSON_STRING)
+            playerJsonStringToPlayer(badJsonString)
         }
     }
     "player should NOT build from BAD JSON 2" {
         shouldThrowAny {
-            playerJsonStringToPlayer(BAD_JSON_STRING_2)
+            playerJsonStringToPlayer(badJsonString2)
         }
     }
     "player should return an updated giftHistory" {
