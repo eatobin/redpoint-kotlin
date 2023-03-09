0a7290bcfcfa5a4a360268fffbef21f5909b4f94
Eric Tobin <eatobin@gmail.com>
2023-03-09 16:35:57 -0700

    reformat vals

--- src/test/kotlin/PlayersTest.kt
+++ src/test/kotlin/PlayersTest.kt
@@ -2,54 +2,54 @@ import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-private const val JSON_STRING: JsonStringTA =
-    "{\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]},\"PauMcc\":{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}}"
-private const val BAD_JSON_STRING: JsonStringTA =
-    "{\"PauMcc\":{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]},\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]}}"
-private const val BAD_JSON_STRING_2: JsonStringTA =
-    "{\"PauMcc\":{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]},\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]}}"
-
-private val rinSta: Player = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
-private val johLen: Player = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
-private val geoHar: Player = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
-private val pauMcc: Player = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
-private val players: PlayersTA =
-    sortedMapOf("PauMcc" to pauMcc, "GeoHar" to geoHar, "RinSta" to rinSta, "JohLen" to johLen)
-
-private val newBee: Player = Player("New Bee", listOf(GiftPair("NewBee", "NewBee")))
-private val newBeePlayers: PlayersTA =
-    sortedMapOf("RinSta" to newBee, "JohLen" to johLen, "GeoHar" to geoHar, "PauMcc" to pauMcc)
-
-private val rinStaExt: Player =
-    Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar"), GiftPair("RinSta", "RinSta")))
-private val johLenExt: Player =
-    Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta"), GiftPair("JohLen", "JohLen")))
-private val geoHarExt: Player =
-    Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc"), GiftPair("GeoHar", "GeoHar")))
-private val pauMccExt: Player =
-    Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen"), GiftPair("PauMcc", "PauMcc")))
-private val playersExt: PlayersTA =
-    sortedMapOf("RinSta" to rinStaExt, "JohLen" to johLenExt, "GeoHar" to geoHarExt, "PauMcc" to pauMccExt)
-
-private val geoHarGivee: Player = Player("George Harrison", listOf(GiftPair("you", "PauMcc")))
-private val geoHarGiver: Player = Player("George Harrison", listOf(GiftPair("RinSta", "you")))
-private val playersGivee: PlayersTA =
-    sortedMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGivee, "PauMcc" to pauMcc)
-private val playersGiver: PlayersTA =
-    sortedMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGiver, "PauMcc" to pauMcc)
-
 class PlayersTest : StringSpec({
+    val jsonString: JsonStringTA =
+        "{\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]},\"PauMcc\":{\"playerName\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]}}"
+    val badJsonString: JsonStringTA =
+        "{\"PauMcc\":{\"playerName\"\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]},\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]}}"
+    val badJsonString2: JsonStringTA =
+        "{\"PauMcc\":{\"playerNameX\":\"Paul McCartney\",\"giftHistory\":[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]},\"GeoHar\":{\"playerName\":\"George Harrison\",\"giftHistory\":[{\"givee\":\"RinSta\",\"giver\":\"PauMcc\"}]},\"JohLen\":{\"playerName\":\"John Lennon\",\"giftHistory\":[{\"givee\":\"PauMcc\",\"giver\":\"RinSta\"}]},\"RinSta\":{\"playerName\":\"Ringo Starr\",\"giftHistory\":[{\"givee\":\"JohLen\",\"giver\":\"GeoHar\"}]}}"
+
+    val rinSta = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
+    val johLen = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
+    val geoHar = Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc")))
+    val pauMcc = Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen")))
+    val players: PlayersTA =
+        sortedMapOf("PauMcc" to pauMcc, "GeoHar" to geoHar, "RinSta" to rinSta, "JohLen" to johLen)
+
+    val newBee = Player("New Bee", listOf(GiftPair("NewBee", "NewBee")))
+    val newBeePlayers: PlayersTA =
+        sortedMapOf("RinSta" to newBee, "JohLen" to johLen, "GeoHar" to geoHar, "PauMcc" to pauMcc)
+
+    val rinStaExt =
+        Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar"), GiftPair("RinSta", "RinSta")))
+    val johLenExt =
+        Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta"), GiftPair("JohLen", "JohLen")))
+    val geoHarExt =
+        Player("George Harrison", listOf(GiftPair("RinSta", "PauMcc"), GiftPair("GeoHar", "GeoHar")))
+    val pauMccExt =
+        Player("Paul McCartney", listOf(GiftPair("GeoHar", "JohLen"), GiftPair("PauMcc", "PauMcc")))
+    val playersExt: PlayersTA =
+        sortedMapOf("RinSta" to rinStaExt, "JohLen" to johLenExt, "GeoHar" to geoHarExt, "PauMcc" to pauMccExt)
+
+    val geoHarGivee = Player("George Harrison", listOf(GiftPair("you", "PauMcc")))
+    val geoHarGiver = Player("George Harrison", listOf(GiftPair("RinSta", "you")))
+    val playersGivee: PlayersTA =
+        sortedMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGivee, "PauMcc" to pauMcc)
+    val playersGiver: PlayersTA =
+        sortedMapOf("RinSta" to rinSta, "JohLen" to johLen, "GeoHar" to geoHarGiver, "PauMcc" to pauMcc)
+
     "players should build a sortedMap from JSON" {
-        playersJsonStringToPlayers(JSON_STRING).shouldBe(players)
+        playersJsonStringToPlayers(jsonString).shouldBe(players)
     }
     "players should NOT build a sortedMap from BAD JSON" {
         shouldThrowAny {
-            playersJsonStringToPlayers(BAD_JSON_STRING)
+            playersJsonStringToPlayers(badJsonString)
         }
     }
     "players should NOT build a sortedMap from BAD JSON 2" {
         shouldThrowAny {
-            playersJsonStringToPlayers(BAD_JSON_STRING_2)
+            playersJsonStringToPlayers(badJsonString2)
         }
     }
     "players should return an updated player" {
