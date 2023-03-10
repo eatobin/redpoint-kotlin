cb01caee20a8f7fcb613a334e62949358f9e62ec
Eric Tobin <eatobin@gmail.com>
2023-03-10 16:28:24 -0700

    MyState.kt progress

--- src/test/kotlin/MyStateTest.kt
+++ src/test/kotlin/MyStateTest.kt
@@ -1,10 +1,15 @@
 import MyState.Companion.myStateJsonStringToMyState
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class MyStateTest : StringSpec({
     val beatlesJson: JsonStringTA =
         """{"rosterName":"The Beatles","rosterYear":2014,"players":{"RinSta":{"playerName":"Ringo Starr","giftHistory":[{"givee":"JohLen","giver":"GeoHar"}]},"JohLen":{"playerName":"John Lennon","giftHistory":[{"givee":"PauMcc","giver":"RinSta"}]},"GeoHar":{"playerName":"George Harrison","giftHistory":[{"givee":"RinSta","giver":"PauMcc"}]},"PauMcc":{"playerName":"Paul McCartney","giftHistory":[{"givee":"GeoHar","giver":"JohLen"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}"""
+    val beatlesJsonBad: JsonStringTA =
+        """{"rosterName""The Beatles","rosterYear":2014,"players":{"RinSta":{"playerName":"Ringo Starr","giftHistory":[{"givee":"JohLen","giver":"GeoHar"}]},"JohLen":{"playerName":"John Lennon","giftHistory":[{"givee":"PauMcc","giver":"RinSta"}]},"GeoHar":{"playerName":"George Harrison","giftHistory":[{"givee":"RinSta","giver":"PauMcc"}]},"PauMcc":{"playerName":"Paul McCartney","giftHistory":[{"givee":"GeoHar","giver":"JohLen"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}"""
+    val beatlesJsonBad2: JsonStringTA =
+        """{"rosterNameX":"The Beatles","rosterYear":2014,"players":{"RinSta":{"playerName":"Ringo Starr","giftHistory":[{"givee":"JohLen","giver":"GeoHar"}]},"JohLen":{"playerName":"John Lennon","giftHistory":[{"givee":"PauMcc","giver":"RinSta"}]},"GeoHar":{"playerName":"George Harrison","giftHistory":[{"givee":"RinSta","giver":"PauMcc"}]},"PauMcc":{"playerName":"Paul McCartney","giftHistory":[{"givee":"GeoHar","giver":"JohLen"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}"""
 
     val rinSta = Player("Ringo Starr", listOf(GiftPair("JohLen", "GeoHar")))
     val johLen = Player("John Lennon", listOf(GiftPair("PauMcc", "RinSta")))
@@ -27,18 +32,17 @@ class MyStateTest : StringSpec({
 
     "MyState should build from JSON" {
         myStateJsonStringToMyState(beatlesJson).shouldBe(beatlesState0)
-//        myToJSON(beatlesState0).shouldBe("XXX")
     }
-//    "player should NOT build from BAD JSON" {
-//        shouldThrowAny {
-//            playerJsonStringToPlayer(badJsonString)
-//        }
-//    }
-//    "player should NOT build from BAD JSON 2" {
-//        shouldThrowAny {
-//            playerJsonStringToPlayer(badJsonString2)
-//        }
-//    }
+    "MyState should NOT build from BAD JSON" {
+        shouldThrowAny {
+            myStateJsonStringToMyState(beatlesJsonBad)
+        }
+    }
+    "MyState should NOT build from BAD JSON 2" {
+        shouldThrowAny {
+            myStateJsonStringToMyState(beatlesJsonBad2)
+        }
+    }
 //    "player should return an updated giftHistory" {
 //        playerUpdateGiftHistory(listOf(GiftPair("nope", "yup")), player).shouldBe(
 //            Player(
