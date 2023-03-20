026495901ee08c8b2a0f23a5d58dd4b138dc061a
Eric Tobin <eatobin@gmail.com>
2023-03-19 20:29:08 -0700

    Main.kt progress

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,23 @@
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-
-@Serializable
-data class Project(val name: String, val language: String)
+import MyState.Companion.myStateAskContinue
+import MyState.Companion.myStateJsonStringToMyState
+import MyState.Companion.myStatePrintResults
+import MyState.Companion.myStateUpdateAndRunNewYear
+import java.util.*
 
 fun main() {
-    // Serializing objects
-    val data = Project("kotlinx.serialization", "Kotlin")
-    val string = Json.encodeToString(data)
-    println(string) // {"name":"kotlinx.serialization","language":"Kotlin"}
-    // Deserializing back into objects
-    val obj = Json.decodeFromString<Project>(string)
-    println(obj) // Project(name=kotlinx.serialization, language=Kotlin)
-    val numbers: List<Int> = listOf(1, 2, 3, 4)
-    val numString: String = Json.encodeToString(numbers)
-    println("numString: $numString")
-    val numbers2: List<Int> = Json.decodeFromString(numString)
-    println("numbers2: $numbers2")
-    val numbers99: List<Int> = Json.decodeFromString(listOf(11, 22, 33, 44).toString())
-    println("numbers99: $numbers99")
+    val hawksJson: JsonStringTA =
+        """{"rosterName":"Blackhawks","rosterYear":2010,"players":{"TroBro":{"playerName":"Troy Brouwer","giftHistory":[{"givee":"DavBol","giver":"JoeQue"}]},"PatKan":{"playerName":"Patrick Kane","giftHistory":[{"givee":"BryBic","giver":"CriHue"}]},"JoeQue":{"playerName":"Joel Quenneville","giftHistory":[{"givee":"TroBro","giver":"AndLad"}]},"NikHja":{"playerName":"Niklas Hjalmarsson","giftHistory":[{"givee":"BreSea","giver":"BriCam"}]},"TomKop":{"playerName":"Tomas Kopecky","giftHistory":[{"givee":"CriHue","giver":"DunKei"}]},"BryBic":{"playerName":"Bryan Bickell","giftHistory":[{"givee":"MarHos","giver":"PatKan"}]},"AntNie":{"playerName":"Antti Niemi","giftHistory":[{"givee":"JonToe","giver":"MarHos"}]},"PatSha":{"playerName":"Patrick Sharp","giftHistory":[{"givee":"BriCam","giver":"DavBol"}]},"DunKei":{"playerName":"Duncan Keith","giftHistory":[{"givee":"TomKop","giver":"AdaBur"}]},"BriCam":{"playerName":"Brian Campbell","giftHistory":[{"givee":"NikHja","giver":"PatSha"}]},"BreSea":{"playerName":"Brent Seabrook","giftHistory":[{"givee":"KriVer","giver":"NikHja"}]},"KriVer":{"playerName":"Kris Versteeg","giftHistory":[{"givee":"AndLad","giver":"BreSea"}]},"MarHos":{"playerName":"Marian Hossa","giftHistory":[{"givee":"AntNie","giver":"BryBic"}]},"AndLad":{"playerName":"Andrew Ladd","giftHistory":[{"givee":"JoeQue","giver":"KriVer"}]},"DavBol":{"playerName":"Dave Bolland","giftHistory":[{"givee":"PatSha","giver":"TroBro"}]},"CriHue":{"playerName":"Cristobal Huet","giftHistory":[{"givee":"PatKan","giver":"TomKop"}]},"JonToe":{"playerName":"Jonathan Toews","giftHistory":[{"givee":"AdaBur","giver":"AntNie"}]},"AdaBur":{"playerName":"Adam Burish","giftHistory":[{"givee":"DunKei","giver":"JonToe"}]}},"giftYear":0,"giveeHat":[],"giverHat":[],"maybeGivee":null,"maybeGiver":null,"discards":[],"quit":"n"}"""
+    val firstState: MyState = myStateJsonStringToMyState(hawksJson)
+    tailrec fun loop(nextState: MyState) {
+        if (nextState.quit.lowercase(Locale.getDefault()) == "q") {
+            println()
+            println("This was fun!")
+            println("Talk about a position with Redpoint?")
+            println("Please call: Eric Tobin 773-679-6617")
+            println()
+        } else {
+            loop(myStateAskContinue(myStatePrintResults(myStateUpdateAndRunNewYear(nextState))))
+        }
+    }
+    loop(myStateAskContinue(myStatePrintResults(firstState)))
 }
